571815f1f335613cea0f4941ccd1b2771551f55f
ShyreneChing <shyrene_ching@dlsu.edu.ph>
2021-08-31 18:50:05 +0800

    rename geojson

--- static/map.js
+++ static/map.js
@@ -4,11 +4,16 @@ var map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/erika-chan/ckszvg5py745v17qudxf0fs8e', // style URL
     center: [120.99, 12.56], // starting position [lng, lat]
-    zoom: 4.5, // starting zoom,
-    minZoom: 4.5,
+    zoom: 4.7, // starting zoom,
+    minZoom: 4.7,
     maxZoom: 13
 });
 
 map.on('load', () => {
     //put things here
+
+    map.addSource('makati', {
+        type: 'geojson',
+        data: 'data/healthsites.geojson'
+    });
 });
