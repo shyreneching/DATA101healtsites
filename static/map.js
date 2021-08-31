61a6de526eb717c908d9169fe1d56332160ec60c
ShyreneChing <shyrene_ching@dlsu.edu.ph>
2021-08-31 22:31:57 +0800

    updated data

--- static/map.js
+++ static/map.js
@@ -54,7 +54,9 @@ map.on('load', () => {
         // Copy coordinates array.
         const coordinates = e.features[0].geometry.coordinates.slice();
         const description = e.features[0].properties.name + 
-                            "<br>Amenity: " + titleCase(e.features[0].properties.amenity);
+                            "<br>Amenity: " + titleCase(e.features[0].properties.amenity) +
+                            "<br>Amenity: " + titleCase(e.features[0].properties.addr_housenumber) +" "+ titleCase(e.features[0].properties.addr_city) +
+                            "<br>Province:" + e.features[0].properties.NAME_1;
 
         // Ensure that if the map is zoomed out such that multiple
         // copies of the feature are visible, the popup appears
