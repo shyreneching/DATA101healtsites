67bcaf2bb6b532c88459a42f1af60a1432d82b28
ErikaChan <43471017+ErikaChan@users.noreply.github.com>
2021-09-11 01:07:03 +0800

    Modified scroll to bottom bounds and scroll target

--- static/scrolltobottom.js
+++ static/scrolltobottom.js
@@ -2,21 +2,21 @@ let btnscroll = document.querySelector('#goToBottom')
 
 btnscroll.addEventListener('click', function () {
   console.log("Hello");
-  var element = document.getElementById("num_healthsites_per_region");
+  var element = document.getElementById("other_charts_description");
   element.scrollIntoView({behavior: "smooth"});
   // btnscroll.style.visibility='hidden';
-  
+
 });
 
 $(document).ready(function () {
   window.onscroll = function() {scrollFunction()};
-		
+
   function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      document.getElementById("goToBottom").style.display = "none";   
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+      document.getElementById("goToBottom").style.display = "none";
     } else {
       document.getElementById("goToBottom").style.display = "block";
     }
   }
-  
+
 })
