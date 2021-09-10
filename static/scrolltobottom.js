bc625a308744b3175c06d5459e32571ec903cb20
ShyreneChing <shyrene_ching@dlsu.edu.ph>
2021-09-10 18:59:47 +0800

    fixed goToBottom disappear

--- static/scrolltobottom.js
+++ static/scrolltobottom.js
@@ -4,5 +4,19 @@ btnscroll.addEventListener('click', function () {
   console.log("Hello");
   var element = document.getElementById("num_healthsites_per_region");
   element.scrollIntoView({behavior: "smooth"});
-  btnscroll.style.visibility='hidden';
+  // btnscroll.style.visibility='hidden';
+  
 });
+
+$(document).ready(function () {
+  window.onscroll = function() {scrollFunction()};
+		
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("goToBottom").style.display = "none";   
+    } else {
+      document.getElementById("goToBottom").style.display = "block";
+    }
+  }
+  
+})
