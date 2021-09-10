6d454a30adcfec106651b19dee0ae814a3f33265
Myles Chan <myleschan00@gmail.com>
2021-09-10 18:57:26 +0800

    filters for tallies is a-okay

--- static/numsites.js
+++ static/numsites.js
@@ -7,4 +7,46 @@ $(document).ready(function(){
         data = data[0]
         $('#healthsites_total').text(numberWithCommas(data[0]))
     })
+
+    $("#select_amentity").on('change', function(){
+        var worker = $("#select_healthcare_worker_type").val()
+        var sector = $('input[name="inlineRadioOptions"]:checked').val();
+        var amenity = $("#select_amentity").val()
+        var location = $("#select_province").val()
+        if((location  == null || location  == "ALL") && ($("#select_region").val() != null && $("#select_region").val() != "ALL")){
+            location = "region_" + $("#select_region").val()
+        }
+
+        d3.csv('/numsites/'+worker+"/"+sector+"/"+amenity+"/"+location).then(function(data){
+            data = data[0]
+            $('#healthsites_total').text(numberWithCommas(data[0]))
+        })
+    })
+
+    $('#select_province').on('change', function(){
+        var worker = $("#select_healthcare_worker_type").val()
+        var sector = $('input[name="inlineRadioOptions"]:checked').val();
+        var amenity = $("#select_amentity").val()
+        var location = $("#select_province").val()
+
+        d3.csv('/numsites/'+worker+"/"+sector+"/"+amenity+"/"+location).then(function(data){
+            data = data[0]
+            $('#healthsites_total').text(numberWithCommas(data[0]))
+        })
+    })
+
+    $('#select_region').on('change', function(){
+        var worker = $("#select_healthcare_worker_type").val()
+        var sector = $('input[name="inlineRadioOptions"]:checked').val();
+        var amenity = $("#select_amentity").val()
+        var location = $("#select_region").val()
+        if(location  != null && location  != "ALL"){
+            location = "region_" + $("#select_region").val()
+        }
+
+        d3.csv('/numsites/'+worker+"/"+sector+"/"+amenity+"/"+location).then(function(data){
+            data = data[0]
+            $('#healthsites_total').text(numberWithCommas(data[0]))
+        })
+    })
 })
--- static/numworkers.js
+++ static/numworkers.js
@@ -17,7 +17,47 @@ $(document).ready(function(){
             location = "region_" + $("#select_region").val()
         }
 
-        console.log("/"+worker+"/"+sector+"/"+amenity+"/"+location)
+        d3.csv('/numworkers/'+worker+"/"+sector+"/"+amenity+"/"+location).then(function(data){
+            data = data[0]
+            $('#healthcare_workers_total').text(numberWithCommas(data[0]))
+        })
+    })
+
+    $('input[name="inlineRadioOptions"]').on('change', function(){
+        var worker = $("#select_healthcare_worker_type").val()
+        var sector = $('input[name="inlineRadioOptions"]:checked').val();
+        var amenity = $("#select_amentity").val()
+        var location = $("#select_province").val()
+        if((location  == null || location  == "ALL") && ($("#select_region").val() != null && $("#select_region").val() != "ALL")){
+            location = "region_" + $("#select_region").val()
+        }
+
+        d3.csv('/numworkers/'+worker+"/"+sector+"/"+amenity+"/"+location).then(function(data){
+            data = data[0]
+            $('#healthcare_workers_total').text(numberWithCommas(data[0]))
+        })
+    })
+
+    $('#select_province').on('change', function(){
+        var worker = $("#select_healthcare_worker_type").val()
+        var sector = $('input[name="inlineRadioOptions"]:checked').val();
+        var amenity = $("#select_amentity").val()
+        var location = $("#select_province").val()
+
+        d3.csv('/numworkers/'+worker+"/"+sector+"/"+amenity+"/"+location).then(function(data){
+            data = data[0]
+            $('#healthcare_workers_total').text(numberWithCommas(data[0]))
+        })
+    })
+
+    $('#select_region').on('change', function(){
+        var worker = $("#select_healthcare_worker_type").val()
+        var sector = $('input[name="inlineRadioOptions"]:checked').val();
+        var amenity = $("#select_amentity").val()
+        var location = $("#select_region").val()
+        if(location  != null && location  != "ALL"){
+            location = "region_" + $("#select_region").val()
+        }
 
         d3.csv('/numworkers/'+worker+"/"+sector+"/"+amenity+"/"+location).then(function(data){
             data = data[0]
