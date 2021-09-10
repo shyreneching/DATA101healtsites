99bfea0405803d3e68181b3ca4c4314fa26daf67
Myles Chan <myleschan00@gmail.com>
2021-09-10 16:40:03 +0800

    fixed arrays for population of dropdown

--- static/numworkers.js
+++ static/numworkers.js
@@ -7,4 +7,16 @@ $(document).ready(function(){
         data = data[0]
         $('#healthcare_workers_total').text(numberWithCommas(data[0]))
     })
+
+    $("#select_healthcare_worker_type").on('change', function(){
+        var worker = $("#select_healthcare_worker_type").val()
+        var sector = $('input[name="inlineRadioOptions"]:checked').val();
+        var amenity = $("#select_amentity").val()
+        var location = $("#select_province").val()
+        if(location == null || location  == ""){
+            location = $("#select_region").val()
+        }
+
+        console.log("/"+worker+"/"+sector+"/"+amenity+"/"+location)
+    })
 })
